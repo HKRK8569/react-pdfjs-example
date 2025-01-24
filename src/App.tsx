24c18681a1c4d9b252bf49f863455fc3333767a0
HKRK8569 <kayann3021@gmail.com>
2025-01-25 03:16:11 +0900

    layoutの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
 import { useEffect, useRef } from "react";
 import * as pdfjs from "pdfjs-dist";
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
   const pdfCanvasRef = useRef<HTMLCanvasElement>(null);
+  const textLayerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const viewPdf = async () => {
       const canvas = pdfCanvasRef.current;
-      if (!canvas) return;
+      const textLayer = textLayerRef.current;
+      const container = containerRef.current;
+      if (!canvas || !textLayer || !container) return;
       const pdf = await pdfjs.getDocument("sample-local.pdf").promise;
       const page = await pdf.getPage(1);
       const scale = 1.0;
       const viewport = page.getViewport({ scale });
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+      container.style.width = `${viewport.width}px`;
+      container.style.height = `${viewport.height}px`;
       const context = canvas.getContext("2d");
       if (!context) return;
-      const outputScale = window.devicePixelRatio || 1;
-      canvas.width = Math.floor(viewport.width * outputScale);
-      canvas.height = Math.floor(viewport.height * outputScale);
-      canvas.style.width = Math.floor(viewport.width) + "px";
-      canvas.style.height = Math.floor(viewport.height) + "px";
 
       const renderContext = {
         canvasContext: context,
         viewport,
       };
       page.render(renderContext);
-
-      // TODO:textLayerなどの追加
     };
     viewPdf();
   }, [pdfCanvasRef]);
 
   return (
-    <div className="h-screen w-screen">
-      <canvas className="mx-auto" ref={pdfCanvasRef} />
+    <div className="h-screen w-screen ">
+      <div ref={containerRef} className="relative mx-auto">
+        <canvas ref={pdfCanvasRef} />
+        <div className="PdfPage__textLayer" ref={textLayerRef} />
+      </div>
     </div>
   );
 }
